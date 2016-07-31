5a486fcea42a43aa9df083e67caa107453196c99
u174762 <sam.panga@hotmail.com>
2016-07-31 16:03:02 +0530

    added via upload
    Acadgild java assignment1-session1 java

---
+++ assignment1_session1/src/assignment1/javaassignment.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+public class javaassignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		       int a,b,c;
+				a=1;b=5;
+				c=a+b;
+				System.out.println(c);			
+
+			}
+
+	}
+
+
